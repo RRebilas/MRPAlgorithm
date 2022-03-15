c3760acb0bb04624b8ddfc92dcbd4ff50323e795
RRebilas <rrafal7@gmail.com>
2022-03-16 00:46:34 +0100

    Display levels of product and its elements

--- src/app/mrp/results-table/results-table.component.ts
+++ src/app/mrp/results-table/results-table.component.ts
@@ -1,24 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MrpRecord } from 'src/app/core/models/MrpRecord';
 
+const ELEMENT_DATA: MrpRecord[] = [
+  {
+    week: 1,
+    grossRequirement: 10,
+    scheduled: 0,
+    available: 20,
+    nettRequirements: 0,
+    plannedOrderReceipts: 0,
+    plannedOrderRelease: 0,
+  },
+  {
+    week: 2,
+    grossRequirement: 15,
+    scheduled: 0,
+    available: 5,
+    nettRequirements: 0,
+    plannedOrderReceipts: 25,
+    plannedOrderRelease: 0,
+  },
+  {
+    week: 3,
+    grossRequirement: 15,
+    scheduled: 25,
+    available: 15,
+    nettRequirements: 0,
+    plannedOrderReceipts: 0,
+    plannedOrderRelease: 0,
+  },
+  {
+    week: 4,
+    grossRequirement: 10,
+    scheduled: 0,
+    available: 5,
+    nettRequirements: 0,
+    plannedOrderReceipts: 0,
+    plannedOrderRelease: 0,
+  },
+  {
+    week: 5,
+    grossRequirement: 15,
+    scheduled: 0,
+    available: 15,
+    nettRequirements: 10,
+    plannedOrderReceipts: 0,
+    plannedOrderRelease: 25,
+  },
+];
 @Component({
   selector: 'app-results-table',
   templateUrl: './results-table.component.html',
   styleUrls: ['./results-table.component.scss'],
 })
-export class ResultsTableComponent implements OnInit {
-  public dataSource: Array<MrpRecord> = [
-    {
-      week: 1,
-      grossRequirement: 10,
-      scheduled: 0,
-      available: 20,
-      nettRequirements: 0,
-      plannedOrderReceipts: 0,
-      plannedOrderRelease: 0,
-    },
+export class ResultsTableComponent {
+  displayedColumns: string[] = [
+    'mrp-week',
+    'mrp-grossRequirements',
+    'mrp-scheduled',
+    'mrp-available',
+    'mrp-nettRequirements',
+    'mrp-plannedOrderReceipts',
+    'mrp-plannedOrderRelease',
   ];
-  constructor() {}
-
-  ngOnInit(): void {}
+  public dataSource = ELEMENT_DATA;
 }
--- src/app/mrp/side-list/side-list.component.ts
+++ src/app/mrp/side-list/side-list.component.ts
@@ -1,15 +1,83 @@
 import { Component, OnInit } from '@angular/core';
+import { AvailableLevels, Product } from 'src/app/core/models/Product';
 
+const PRODUCT_PARTS: Product[] = [
+  {
+    name: 'Rower',
+    level: AvailableLevels.zero,
+  },
+  {
+    name: 'Koła',
+    level: AvailableLevels.one,
+  },
+  {
+    name: 'Hamulce',
+    level: AvailableLevels.one,
+  },
+  {
+    name: 'Rama',
+    level: AvailableLevels.one,
+  },
+  {
+    name: 'Napęd',
+    level: AvailableLevels.one,
+  },
+  {
+    name: 'Szprychy',
+    level: AvailableLevels.two,
+  },
+  {
+    name: 'Opona',
+    level: AvailableLevels.two,
+  },
+  {
+    name: 'Linka',
+    level: AvailableLevels.two,
+  },
+  {
+    name: 'Zaciski',
+    level: AvailableLevels.two,
+  },
+  {
+    name: 'Sztyca',
+    level: AvailableLevels.two,
+  },
+  {
+    name: 'Widelec',
+    level: AvailableLevels.two,
+  },
+  {
+    name: 'Przerzutka',
+    level: AvailableLevels.two,
+  },
+  {
+    name: 'Pedały',
+    level: AvailableLevels.two,
+  },
+  {
+    name: 'Korba',
+    level: AvailableLevels.two,
+  },
+  {
+    name: 'Łańcuch',
+    level: AvailableLevels.two,
+  },
+  {
+    name: 'Zębatki',
+    level: AvailableLevels.two,
+  },
+];
+
+const PRODUCT_LEVELS: number[] = [0, 1, 2];
 @Component({
   selector: 'app-side-list',
   templateUrl: './side-list.component.html',
-  styleUrls: ['./side-list.component.scss']
+  styleUrls: ['./side-list.component.scss'],
 })
 export class SideListComponent implements OnInit {
+  public data = PRODUCT_PARTS;
+  public levels = PRODUCT_LEVELS;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
